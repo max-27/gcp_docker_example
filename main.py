bd6faa16f0d579f2cad6cdb8dbcb4968a14dfc98
maximilian.frantzen@unternehmertum.de <maximilian.frantzen@unternehmertum.de>
2022-01-11 13:58:51 +0100

    test

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 
 if __name__ == '__main__':
     digits = datasets.load_digits()
-
+    # SOMETHING TO COMMIT
     # flatten the images
     n_samples = len(digits.images)
     data = digits.images.reshape((n_samples, -1))
